5be7f7032aba9e22676347cf3c137decebd8ccd7
somnas <erik.somnas@gmail.com>
2020-12-27 18:07:21 +0100

    routing pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
+import NavlinkItem from './components/NavlinkItem';
 import BuildPage from './pages/BuildPage';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <div className="row">
+        <div className="col-md-12">
+          <ul className="nav">          
+            <NavlinkItem to="/" text="Home" />  
+            <NavlinkItem to="/builder" text="Burger Builder" />        
+          </ul>
+        </div>        
+      </div>
       <h1>BURGER APP</h1>
 
       <Switch>        
